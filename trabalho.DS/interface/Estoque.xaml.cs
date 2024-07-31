3fcff263d8ee88e0079819f5c4994b54a22c0dc9
maria <mariaangelicafreitas76@gmail.com>
2024-07-31 09:53:36 -0300

    interfaces-(sem dot.net)

---
+++ trabalho.DS/interface/Estoque.xaml.cs
@@ -0,0 +1,37 @@
+using Microsoft.Maui.Controls;
+using System;
+
+namespace trabalho.DS
+{
+    public partial class Estoque : ContentPage
+    {
+        public Estoque()
+        {
+            InitializeComponent();
+            ExcluirCommand = new Command(OnExcluirCommandExecuted);
+        }
+
+        public Command ExcluirCommand { get; }
+
+        private void OnExcluirCommandExecuted()
+        {
+            // Exibir o Frame de confirmação
+            ConfirmacaoFrame.IsVisible = true;
+        }
+
+        private void OnConfirmButtonClicked(object sender, EventArgs e)
+        {
+            // Lógica para confirmar a exclusão do material
+            DisplayAlert("Confirmado", "Material excluído", "OK");
+
+            // Ocultar o Frame de confirmação
+            ConfirmacaoFrame.IsVisible = false;
+        }
+
+        private void OnCancelButtonClicked(object sender, EventArgs e)
+        {
+            // Ocultar o Frame de confirmação sem excluir o material
+            ConfirmacaoFrame.IsVisible = false;
+        }
+    }
+}
---
+++ trabalho.DS/interface/Matriais.xaml.cs
@@ -0,0 +1,28 @@
+using Microsoft.Maui.Controls;
+using System;
+
+namespace trabalho.DS
+{
+    public partial class Materiais : ContentPage
+    {
+        public Materiais()
+        {
+            InitializeComponent();
+        }
+
+        private void OnMenuButtonClicked(object sender, EventArgs e)
+        {
+            // Lógica para o botão de menu
+        }
+
+        private void OnBackButtonClicked(object sender, EventArgs e)
+        {
+            // Lógica para o botão de voltar
+        }
+
+        private void OnForwardButtonClicked(object sender, EventArgs e)
+        {
+            // Lógica para o botão de avançar
+        }
+    }
+}
